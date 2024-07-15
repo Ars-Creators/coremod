5253af36f4cfd4a878fe8a85c10d422609346c1f
Meteorcassy <elch200731@gmail.com>
2024-07-15 17:53:42 +0200

    items

--- src/main/java/net/castro/coremod/CoreMod.java
+++ src/main/java/net/castro/coremod/CoreMod.java
@@ -1,6 +1,7 @@
 package net.castro.coremod;
 
 import com.mojang.logging.LogUtils;
+import net.castro.coremod.item.ModItems;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -18,6 +19,8 @@ public class CoreMod {
 
     public CoreMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
---
+++ src/main/java/net/castro/coremod/item/ModItems.java
@@ -0,0 +1,25 @@
+package net.castro.coremod.item;
+
+import net.castro.coremod.CoreMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, CoreMod.MOD_ID);
+
+    public static final RegistryObject<Item> ZIRCON = ITEMS.register("zircon",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.CORE_MOD)));
+
+    public static final RegistryObject<Item> RAW_ZIRCON = ITEMS.register("raw_zircon",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.CORE_MOD)));
+
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
