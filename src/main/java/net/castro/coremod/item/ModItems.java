7a82957f9508eadb80a0d31b09b6a015cddb211e
Meteorcassy <elch200731@gmail.com>
2024-07-16 12:20:05 +0200

    Progress

--- src/main/java/net/castro/coremod/item/ModItems.java
+++ src/main/java/net/castro/coremod/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.castro.coremod.item;
 
 import net.castro.coremod.CoreMod;
+import net.castro.coremod.item.custom.EightBallItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -16,6 +17,9 @@ public class ModItems {
 
     public static final RegistryObject<Item> RAW_ZIRCON = ITEMS.register("raw_zircon",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.CORE_MOD)));
+
+    public static final RegistryObject<Item> EIGHT_BALL = ITEMS.register("eight_ball",
+            () -> new EightBallItem(new Item.Properties().tab(ModCreativeModeTab.CORE_MOD).stacksTo(1)));
 
 
 
